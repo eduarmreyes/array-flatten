66501ba0cb9298aa0437070f52c6981fef93ae0a
eduarmreyes <eduar.mreyes@gmail.com>
2019-06-28 07:28:07 -0400

    add a better if is Array logic

--- bin/process.js
+++ bin/process.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 const arrayExample = [2, 3, [3, 1, 5], [[[0], 0, 5]], [3]];
+// const arrayExample = { name: "Eduardo" };
 // eslint-disable-next-line no-extend-native
 Array.prototype.flatten = function flattenFn() {
   let newArray = [];
   this.forEach(element => {
-    if (typeof element === "object") {
+    if (Array.isArray(element)) {
       newArray = newArray.concat(element.flatten());
     }
     if (typeof element === "number" || typeof element === "string") {
@@ -15,5 +16,7 @@ Array.prototype.flatten = function flattenFn() {
 };
 
 (function startProcess() {
-  console.log(arrayExample.flatten());
+  console.log(
+    Array.isArray(arrayExample) ? arrayExample.flatten() : "Not an array"
+  );
 })();
